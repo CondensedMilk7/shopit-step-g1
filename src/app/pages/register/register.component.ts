a67adf09eac942c8767ce535dfa16ecbe3189982
Pridon Tetradze <pridontetradze@gmail.com>
2023-06-27 19:49:53 +0400

    add confirm password field and validation messages

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import {
+  AbstractControl,
   FormBuilder,
-  FormControl,
   NonNullableFormBuilder,
+  ValidationErrors,
+  ValidatorFn,
   Validators,
 } from '@angular/forms';
 
@@ -20,16 +22,26 @@ export class RegisterComponent {
         Validators.pattern(/^[A-z]/),
         Validators.minLength(2),
         Validators.maxLength(20),
+        this.badNameValidator('badname'),
       ],
     ],
     email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(8)]],
     positions: this.fb.array([this.fb.control('')]),
+    password: ['', [Validators.required, Validators.minLength(8)]],
+    confirmPassword: [''],
   });
 
   // გამოიყენეთ NonNullableFormBuilder რომ reset()-მა
   // null-ის მაგივრად დააყენოს საწყისი მნიშვნელობები
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: NonNullableFormBuilder) {
+    this.controls['confirmPassword'].setValidators(
+      this.confirmPasswordValidator(this.controls['password'])
+    );
+  }
+
+  get controls() {
+    return this.registerForm.controls;
+  }
 
   get positions() {
     return this.registerForm.controls['positions'];
@@ -50,5 +62,25 @@ export class RegisterComponent {
 
   addPosition() {
     this.positions.push(this.fb.control(''));
+  }
+
+  badNameValidator(pattern: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      return control.value.includes(pattern)
+        ? { badName: 'Contains a bad name!' }
+        : null;
+    };
+  }
+
+  confirmPasswordValidator(compareTo: AbstractControl): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (!control.value) {
+        return { required: 'Please confirm password!' };
+      }
+      if (control.value !== compareTo.value) {
+        return { confirmPassword: 'The passwords do not match!' };
+      }
+      return null;
+    };
   }
 }
